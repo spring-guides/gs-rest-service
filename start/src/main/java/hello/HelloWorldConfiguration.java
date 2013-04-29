8e2e25af10cbf5c7e0b7f4b7981afa8e3d7b9c23
Craig Walls <cwalls@vmware.com>
2013-04-29 10:42:08 -0500

    Align start project with true start point of guide.

--- start/src/main/java/hello/HelloWorldConfiguration.java
+++ start/src/main/java/hello/HelloWorldConfiguration.java
@@ -1,4 +1,11 @@
 package hello;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
 public class HelloWorldConfiguration {
 } 
--- start/src/main/java/hello/HelloWorldWebAppInitializer.java
+++ start/src/main/java/hello/HelloWorldWebAppInitializer.java
@@ -1,4 +1,22 @@
 package hello;
 
-public class HelloWorldWebAppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class HelloWorldWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { HelloWorldConfiguration.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
 }
