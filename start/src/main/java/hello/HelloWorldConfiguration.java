4a388c89443628f3c22af0890a75c5f01b84cc9b
Dave Syer <dsyer@vmware.com>
2013-05-08 06:54:18 +0100

    Initial runnable app working with Maven and Gradle deps

--- start/src/main/java/hello/HelloWorldConfiguration.java
+++ start/src/main/java/hello/HelloWorldConfiguration.java
@@ -1,11 +1,19 @@
 package hello;
 
+import org.springframework.bootstrap.SpringApplication;
+import org.springframework.bootstrap.context.annotation.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableAutoConfiguration
 @EnableWebMvc
 @ComponentScan
 public class HelloWorldConfiguration {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldConfiguration.class, args);
+	}
+
 } 
