d0ae283b5cdb79039962edddc64b196025809fa0
Dave Syer <dsyer@vmware.com>
2013-06-04 09:25:49 +0100

    Support for executable war and war packaging
    Net change is a lot more stuff in the parent pom, plus one more dependency
    to handle the provided scope of the embedded container.
    
    * Packaging is no war, and a Application is a WebApplicationInitializer
    * Runs in WTP, and also the war that you get from "mvn package" is
    deployable in a traditional (Servlet 3) container
    * Also runs with "java -jar target/*.war"
    * And "mvn jetty:run" or "mvn tomcat7:run"

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,13 +2,20 @@ package hello;
 
 import org.springframework.bootstrap.SpringApplication;
 import org.springframework.bootstrap.context.annotation.EnableAutoConfiguration;
+import org.springframework.bootstrap.web.BootstrapServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+public class Application extends BootstrapServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { Application.class };
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
 }
