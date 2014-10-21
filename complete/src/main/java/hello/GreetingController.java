36fea0542e5a012f137ee2947d233c55b27aa786
Dave Syer <dsyer@pivotal.io>
2014-10-20 17:21:59 -0700

    Remove redundant required=false
    Fixes gh-26

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
