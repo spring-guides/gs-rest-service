49ca5a4d17dd892bb963240fc63c641a4c48ead0
Brian Clozel <bclozel@pivotal.io>
2015-10-04 23:10:04 +0200

    Prepare guide for "depoy on PWS" feature
    * push app artifact on S3 in CI script
    * add a metadata file "run-on-pws.json" for the deploy agent
    to read the app configuration from

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,20 @@
 cd $(dirname $0)
+cd ../initial
+
+mvn clean compile
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./gradlew compileJava
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
 cd ../complete
 mvn clean package
 java -jar target/gs-rest-service-0.1.0.jar &
@@ -19,6 +35,14 @@ if diff -w ../test/expected.json target/actual.json
         let ret=255
         exit $ret
 fi
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; 
+  then 
+    curl https://raw.githubusercontent.com/timkay/aws/master/aws -o aws
+    chmod u+x aws
+    ./aws put --progress "x-amz-acl: public-read" springio-guides/gs-rest-service-0.1.0.jar target/gs-rest-service-0.1.0.jar
+fi
+
 rm -rf target
 
 ./gradlew build
@@ -26,22 +50,6 @@ ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
 fi
+
 rm -rf build
-
-cd ../initial
-
-mvn clean compile
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
-
-./gradlew compileJava
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
-
 exit
