2e6e5371db92248024feb772b930ef5f09fd27bf
Jonathan LALOU <Jonathan.Lalou@gmail.com>
2017-07-16 00:07:29 -0400

    added application.yml + implemented methods

--- src/main/java/fr/sayasoft/fake/zinc/FakeZincController.java
+++ src/main/java/fr/sayasoft/fake/zinc/FakeZincController.java
@@ -1,5 +1,6 @@
 package fr.sayasoft.fake.zinc;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,36 @@ import java.util.concurrent.atomic.AtomicLong;
 public class FakeZincController {
 
     private static final String template = "Hello, %s!";
+    public static final String GET_ORDER_RESPONSE = "{\n" +
+            "  \"_type\" : \"order_response\",\n" +
+            "  \"price_components\" : {\n" +
+            "    \"shipping\" : 0,\n" +
+            "    \"subtotal\" : 1999,\n" +
+            "    \"tax\" : 0,\n" +
+            "    \"total\" : 1999\n" +
+            "  },\n" +
+            "  \"merchant_order_ids\" : [\n" +
+            "    {\n" +
+            "      \"merchant_order_id\" : \"112-1234567-7272727\",\n" +
+            "      \"merchant\" : \"amazon\",\n" +
+            "      \"account\" : \"timbeaver@gmail.com\",\n" +
+            "      \"placed_at\" : ISODate(\"2014-07-02T23:51:08.366Z\")\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"tracking\" : [\n" +
+            "    {\n" +
+            "      \"merchant_order_id\" : \"112-1234567-7272727\",\n" +
+            "      \"carrier\" : \"Fedex\",\n" +
+            "      \"tracking_number\" : \"9261290100129790891234\",\n" +
+            "      \"obtained_at\" : ISODate(\"2014-07-03T23:22:48.165Z\")\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}";
+
+    public static final String POST_ORDER_RESPONSE = "{\n" +
+            "  \"request_id\": \"3f1c939065cf58e7b9f0aea70640dffc\"\n" +
+            "}";
+
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
@@ -22,13 +53,21 @@ public class FakeZincController {
                 String.format(template, name));
     }
 
-    @RequestMapping(value = "/order/{request_id}", method = RequestMethod.GET,)
+    @RequestMapping(
+            value = "/order/{request_id}",
+            method = RequestMethod.GET,
+            produces = "application/json; charset=UTF-8"
+    )
     public String getOrder(@PathVariable(value = "request_id") String requestId) {
-        return null;
+        return GET_ORDER_RESPONSE;
     }
 
-    @RequestMapping(value = "/order", method = RequestMethod.POST)
+    @RequestMapping(
+            value = "/order",
+            method = RequestMethod.POST,
+            produces = "application/json; charset=UTF-8"
+    )
     public ResponseEntity<?> postOrder(@RequestBody Object requestBody) {
-        return null;
+        return new ResponseEntity<>(POST_ORDER_RESPONSE, HttpStatus.CREATED);
     }
 }
