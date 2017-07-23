f4f7134d91fdc269b2543eb3046cd6dd3eea5a52
Jonathan LALOU <Jonathan.Lalou@gmail.com>
2017-07-23 01:09:05 -0400

    renamed package

--- src/main/java/fr/sayasoft/fake/zinc/FakeZincController.java
+++ src/main/java/fr/sayasoft/fake/zinc/FakeZincController.java
@@ -1,6 +1,6 @@
 package fr.sayasoft.fake.zinc;
 
-import fr.sayasoft.zinc.sdk.pogo.OrderRequest;
+import fr.sayasoft.zinc.sdk.domain.OrderRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
