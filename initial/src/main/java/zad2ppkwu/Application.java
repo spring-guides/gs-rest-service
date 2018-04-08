156ae679bbff75846eda4bff7741dd562b2b2cc4
smoczynskaj <smoczynska.justyna@gmail.com>
2018-04-08 10:18:21 +0200

    Zad 2. Dodanie klasy Application

---
+++ initial/src/main/java/zad2ppkwu/Application.java
@@ -0,0 +1,16 @@
+package zad2ppkwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Justyna on 08.04.2018.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
