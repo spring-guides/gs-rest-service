253ef8bf6a68f5c78a3d853eee48f83965f8b43a
smoczynskaj <smoczynska.justyna@gmail.com>
2018-04-08 12:03:33 +0200

    Zad 2. Dodanie kotrolera i modyfikacja pól

--- initial/src/main/java/zad2ppkwu/Fields.java
+++ initial/src/main/java/zad2ppkwu/Fields.java
@@ -1,15 +1,43 @@
 package zad2ppkwu;
 
+import javafx.scene.control.Hyperlink;
+
+import javax.validation.constraints.Size;
+
 /**
  * Created by Justyna on 08.04.2018.
  */
 public class Fields {
     private long id;
+
+    @Size(min = 5, message = "E-mail must follow the pattern: address@example.com")
     private String mail;
-    private int nip;
-    private int pesel;
-    private int regon;
-    private char postcode;
+
+    @Size(min = 10, max = 10, message = "NIP value must contain 10 digits")
+    private long nip;
+
+    @Size(min = 11, max = 11, message = "PESEL value must contain 11 digits")
+    private long pesel;
+
+    @Size(min = 9, max = 14, message = "REGON value must contain 9 or 14 digits")
+    private long regon;
+
+    @Size(min = 6, max = 6, message = "Postcode must follow the pattern: XX-XXX")
+    private String postcode;
+
+    public Fields() {
+        super();
+    }
+
+    public Fields(long id, String mail, long nip, long pesel, long regon, String postcode) {
+        super();
+        this.id = id;
+        this.mail = mail;
+        this.nip = nip;
+        this.pesel = pesel;
+        this.regon = regon;
+        this.postcode = postcode;
+    }
 
     public long getId() {
         return id;
@@ -27,35 +55,35 @@ public class Fields {
         this.mail = mail;
     }
 
-    public int getNip() {
+    public long getNip() {
         return nip;
     }
 
-    public void setNip(int nip) {
+    public void setNip(long nip) {
         this.nip = nip;
     }
 
-    public int getPesel() {
+    public long getPesel() {
         return pesel;
     }
 
-    public void setPesel(int pesel) {
+    public void setPesel(long pesel) {
         this.pesel = pesel;
     }
 
-    public int getRegon() {
+    public long getRegon() {
         return regon;
     }
 
-    public void setRegon(int regon) {
+    public void setRegon(long regon) {
         this.regon = regon;
     }
 
-    public char getPostcode() {
+    public String getPostcode() {
         return postcode;
     }
 
-    public void setPostcode(char postcode) {
+    public void setPostcode(String  postcode) {
         this.postcode = postcode;
     }
 }
--- initial/src/main/java/zad2ppkwu/FormController.java
+++ initial/src/main/java/zad2ppkwu/FormController.java
@@ -1,7 +1,17 @@
 package zad2ppkwu;
 
+import javafx.scene.control.Hyperlink;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * Created by Justyna on 08.04.2018.
  */
+
+@RestController
 public class FormController {
+
+    @RequestMapping(value = "/fields", method = RequestMethod.POST)
+    public Fields fields(@RequestParam Fields fields) {
+        return new Fields(1, "jankowalski@example.com", Long.parseLong("0123456789"), 12123423456L, Long.parseLong("098472839"), "01-100");
+    }
 }
