75caff1933ec2b7092fc794731911a54aefea9a9
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:09:06 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/test/java/hello/GreetingControllerTests.java
+++ complete/src/test/java/hello/GreetingControllerTests.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *	  https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -33,22 +33,22 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class GreetingControllerTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Test
-    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+	@Test
+	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
-        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, World!"));
-    }
+		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.content").value("Hello, World!"));
+	}
 
-    @Test
-    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+	@Test
+	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
 
-        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
-    }
+		this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+				.andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+	}
 
 }
