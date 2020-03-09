33f0db4ed64dd95b19d3f96cd534e87d09ca39b5
Vincent Biret <vibiret@microsoft.com>
2020-03-09 16:22:29 -0400

    - adds notification controller

---
+++ src/main/java/com/example/restservice/NotificationController.java
@@ -0,0 +1,24 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+
+    @GetMapping("/notification")
+    @ResponseBody
+    public String subscribe() {
+        return "Subscribed to entity with subscription id";
+    }
+    @PostMapping("/notification")
+    @ResponseBody
+	public String handleNotification(@RequestParam(value = "validationToken", defaultValue = "") String validationToken) {
+		return validationToken;
+	}
+}
