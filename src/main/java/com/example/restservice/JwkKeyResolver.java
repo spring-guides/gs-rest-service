3cdbdeb5951ce0e31149827149820fc360be63a6
Vincent Biret <vincentbiret@hotmail.com>
2020-03-13 16:37:33 -0400

    - adds token validation

---
+++ src/main/java/com/example/restservice/JwkKeyResolver.java
@@ -0,0 +1,32 @@
+package com.example.restservice;
+
+import com.auth0.jwk.JwkProvider;
+import com.auth0.jwk.UrlJwkProvider;
+import com.auth0.jwk.Jwk;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwsHeader;
+import io.jsonwebtoken.SigningKeyResolverAdapter;
+import java.security.Key;
+import java.net.URI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JwkKeyResolver extends SigningKeyResolverAdapter {
+    // private static String wellKnownUri = "https://login.microsoftonline.com/common/.well-known/openid-configuration";
+    private JwkProvider keyStore;
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    public JwkKeyResolver() throws java.net.URISyntaxException, java.net.MalformedURLException {
+        this.keyStore = new UrlJwkProvider((new URI("https://login.microsoftonline.com/common/discovery/keys").toURL()));
+    }
+    @Override
+    public Key resolveSigningKey(JwsHeader jwsHeader, Claims claims) {
+        try {
+            String keyId = jwsHeader.getKeyId();
+            Jwk pub = keyStore.get(keyId);
+            return pub.getPublicKey();
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            return null;
+        }
+    }
+}
--- src/main/java/com/example/restservice/NotificationController.java
+++ src/main/java/com/example/restservice/NotificationController.java
@@ -28,6 +28,9 @@ import java.security.cert.*;
 import org.apache.commons.codec.binary.Base64;
 import javax.crypto.*;
 import javax.crypto.spec.*;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @RestController
 public class NotificationController {
@@ -37,7 +40,7 @@ public class NotificationController {
     private String tenantId = "bd4c6c31-c49c-4ab6-a0aa-742e07c20232";
 //endregion
 //region subscription information
-    private String publicUrl = "https://e7ff1771.ngrok.io";
+    private String publicUrl = "https://c2ddde53.ngrok.io";
     private String resource = "teams/9c05f27f-f866-4cc0-b4c2-6225a4568bc5/channels/19:015c392a3030451f8b52fac6084be56d@thread.skype/messages";
 //endregion
 //region certificate information
@@ -49,6 +52,7 @@ public class NotificationController {
     private NationalCloud endpoint = NationalCloud.Global;
     private List scopes = Arrays.asList("https://graph.microsoft.com/.default");
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    private JwkKeyResolver jwksResolver;
     @GetMapping("/notification")
     @ResponseBody
     public String subscribe() throws KeyStoreException, FileNotFoundException, IOException, CertificateException, NoSuchAlgorithmException {
@@ -120,6 +124,33 @@ public class NotificationController {
         cipher.init(Cipher.DECRYPT_MODE, skey, ivspec);
         return new String(cipher.doFinal(Base64.decodeBase64(encryptedData)));
     }
+    private boolean IsValidationTokenValid(String[] validAudiences, String[] validTenantIds, String serializedToken) {
+        try {
+            if(this.jwksResolver == null) {
+                this.jwksResolver = new JwkKeyResolver();
+            }
+            Jws<Claims> token = Jwts.parserBuilder()
+                .setSigningKeyResolver(this.jwksResolver)
+                .build()
+                .parseClaimsJws(serializedToken);
+            Claims body = token.getBody();
+            String audience = body.getAudience();
+            boolean isAudienceValid = false;
+            for(String validAudience : validAudiences) {
+                isAudienceValid = isAudienceValid || validAudience.equals(audience);
+            }
+            String issuer = body.getIssuer();
+            boolean isTenantValid = false;
+            for(String validTenantId : validTenantIds) {
+                isTenantValid = isTenantValid || issuer.endsWith(validTenantId + "/");
+            }
+            return isAudienceValid  && isTenantValid; //nbf,exp and signature are already validated by library
+        } catch (Exception e) {
+            LOGGER.error("could not validate token");
+            LOGGER.error(e.getMessage());
+            return false;
+        }
+    }
     @PostMapping(value="/notification", headers = {"content-type=text/plain"})
     @ResponseBody
     public ResponseEntity<String> handleValidation(@RequestParam(value = "validationToken") String validationToken){
@@ -131,13 +162,19 @@ public class NotificationController {
         LOGGER.info(notifications.value.get(0).resource);
         byte[] decryptedKey = this.GetEncryptionKey(notifications.value.get(0).encryptedContent.dataKey);
         boolean isDataSignatureValid = this.IsDataSignatureValid(decryptedKey, notifications.value.get(0).encryptedContent.data, notifications.value.get(0).encryptedContent.dataSignature);
-        if(isDataSignatureValid) {
+        boolean areValidationTokensValid = true;
+        for(String validationToken : notifications.validationTokens) {
+            areValidationTokensValid = areValidationTokensValid && this.IsValidationTokenValid(new String[] {this.clientId}, new String[]{this.tenantId}, validationToken);
+        }
+        if(areValidationTokensValid && isDataSignatureValid) {
             String decryptedData = this.GetDecryptedData(decryptedKey, notifications.value.get(0).encryptedContent.data);
             LOGGER.info("decrypted data");
             LOGGER.info(decryptedData);
             return ResponseEntity.ok().body("");
-        } else {
+        } else if(!isDataSignatureValid) {
             return ResponseEntity.badRequest().body("data signature validation failed");
+        } else {
+            return ResponseEntity.badRequest().body("token validation failed");
         }
 	}
 }
