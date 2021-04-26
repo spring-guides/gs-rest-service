c560cbd9e6d89a9e310ae4fceb7e5cbb9460967b
Greg L. Turnquist <gturnquist@vmware.com>
2021-04-26 18:37:29 -0500

    Migrate from master to main.

--- test/run.sh
+++ test/run.sh
@@ -20,7 +20,7 @@ mvn clean package
 
 # if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; 
 #   then 
-#     curl https://raw.githubusercontent.com/timkay/aws/master/aws -o aws
+#     curl https://raw.githubusercontent.com/timkay/aws/main/aws -o aws
 #     chmod u+x aws
 #     ./aws put --progress "x-amz-acl: public-read" springio-guides/gs-rest-service-0.1.0.jar target/gs-rest-service-0.1.0.jar
 # fi
