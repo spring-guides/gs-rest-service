94f3917928de4652f1f204a474e8272516c50112
Daniel Celentano <dacelent@redhat.com>
2021-09-03 11:43:31 -0300

    final 22
    Signed-off-by: Daniel Celentano <dacelent@redhat.com>

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,9 +2,10 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.*;
 
 @SpringBootApplication
-public class RestServiceApplication {
+public class RestServiceApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
