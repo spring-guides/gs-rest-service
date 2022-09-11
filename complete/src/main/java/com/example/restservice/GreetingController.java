425c9ba811cacb1293ea452d68ffc4f9be170393
Keerthan22-sys <keerthangowdas222@gmail.com>
2022-09-12 01:02:55 +0530

    Update GreetingController.java

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/greeting")
+	@GetMapping("/greeting") //Greeting the user
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
