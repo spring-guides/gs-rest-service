21903d96a2d153aab8c7606ab270ba73951a606b
rsime <r.simek@outlook.cz>
2023-02-11 15:32:20 +0100

    1102

---
+++ apache-derby-example/src/main/java/com/javatpoint/apachederbyexample/ApacheDerbyExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatpoint.apachederbyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheDerbyExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheDerbyExampleApplication.class, args);
+	}
+
+}
