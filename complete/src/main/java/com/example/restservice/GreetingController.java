c86bc7b81b8b8636fa575931b8e6713324711ba4
Keisuke <tkeisuke318@gmail.com>
2023-04-18 00:23:12 +0900

    テストコミット

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -13,6 +13,7 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
+	//テスト
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
