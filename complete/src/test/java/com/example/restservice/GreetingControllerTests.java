ade0691d4bfd1fec82354b7b6bdb3e13fb863a67
fguilfuchi <fguilfuchijr@gmail.com>
2023-06-07 12:45:39 -0400

    Update GreetingControllerTests.java

--- complete/src/test/java/com/example/restservice/GreetingControllerTests.java
+++ complete/src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -36,9 +36,10 @@ public class GreetingControllerTests {
 
 	@Test
 	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-
+    	
 		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, World!"));
+           			 .andExpect(jsonPath("$.content").value("Hello, World!"));
+}
 	}
 
 	@Test
