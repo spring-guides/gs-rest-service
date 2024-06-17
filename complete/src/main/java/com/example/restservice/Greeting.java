bb1bd4fa622c7b7729f3b6ed8c028cf87200f1d7
72mask <ljy1017010@163.com>
2024-06-17 17:14:34 +0800

    Update Greeting.java
    Direct running will report an error, need to supplement the complete code

--- complete/src/main/java/com/example/restservice/Greeting.java
+++ complete/src/main/java/com/example/restservice/Greeting.java
@@ -1,3 +1,19 @@
 package com.example.restservice;
 
-public record Greeting(long id, String content) { }
+public class Greeting {
+    private long id;
+    private String content;
+
+    public Greeting(long aId, String aContent) {
+        id = aId;
+        content = aContent;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
