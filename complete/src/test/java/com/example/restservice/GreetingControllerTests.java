a017ea53f2a3e8fe3ae3da9b69b6941c8a3ddc14
Lucas Brena <lucaslbrena@gmail.com>
2025-03-16 12:36:15 -0300

    tarefa extra classe aula.01

--- complete/src/test/java/com/example/restservice/GreetingControllerTests.java
+++ complete/src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// commit Bonus: Copilot
 package com.example.restservice;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
