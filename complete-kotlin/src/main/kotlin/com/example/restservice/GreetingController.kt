5bcace35d12849ec4cd5da94c33cbaa5e27cb682
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-11-20 16:11:45 +0100

    Use top level private const val in Kotlin

--- complete-kotlin/src/main/kotlin/com/example/restservice/GreetingController.kt
+++ complete-kotlin/src/main/kotlin/com/example/restservice/GreetingController.kt
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+private const val template = "Hello, %s!"
+
 @RestController
 class GreetingController {
 
-    companion object {
-        private const val template = "Hello, %s!"
-    }
     private val counter = AtomicLong()
 
     @GetMapping("/greeting")
