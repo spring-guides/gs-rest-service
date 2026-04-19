a9e8eb584f93b9a4f838f14ee954613355ab66f9
seeu0 <singh70050@gmail.com>
2026-04-19 11:48:43 +0530

    Update RestServiceApplication.java
    Fixed: Improved name Handling in Greeting Endpoint

--- initial/src/main/java/com/example/restservice/RestServiceApplication.java
+++ initial/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,7 +2,6 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class RestServiceApplication {
 
